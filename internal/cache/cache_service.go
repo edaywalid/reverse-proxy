64c9afb17304a2ffadd091441859d39c638ef898
edaywalid <becharwalid9@gmail.com>
2024-09-25 22:08:27 +0100

    implement cache-service

---
+++ internal/cache/cache_service.go
@@ -0,0 +1,37 @@
+package cache
+
+import (
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/edaywalid/reverse-proxy/pkg/utils"
+)
+
+type cacheService struct {
+	cache *Cache
+}
+
+func NewCacheService(cache *Cache) *cacheService {
+	return &cacheService{
+		cache: cache,
+	}
+}
+
+func (s *cacheService) Get(key string) (*CacheItem, bool) {
+
+	item, ok := s.cache.GetItem(key)
+	if !ok || time.Since(item.Created()) > utils.CacheExpiration {
+		return nil, false
+	}
+
+	return &item, ok
+}
+
+func (s *cacheService) Set(key string, resp *http.Response) {
+	body, _ := io.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	item := NewCacheItem(body, resp.Header, resp.StatusCode)
+	s.cache.SetItem(key, *item)
+}
