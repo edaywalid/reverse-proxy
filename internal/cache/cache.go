78a71b8250ffd25e01ff0fa85adb7e1d2e884e8b
edaywalid <becharwalid9@gmail.com>
2024-09-25 22:07:04 +0100

    define cache , cache-item models

---
+++ internal/cache/cache.go
@@ -0,0 +1,68 @@
+package cache
+
+import (
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/edaywalid/reverse-proxy/pkg/utils"
+)
+
+type CacheItem struct {
+	/*
+		store the response body as a byte slice
+		because the response body is an io.ReadCloser
+		and it can be read only once
+	*/
+	Body    []byte
+	Headers http.Header
+	Status  int
+	created time.Time
+}
+
+type Cache struct {
+	items map[string]CacheItem
+	lock  sync.RWMutex
+}
+
+func NewCache() *Cache {
+	return &Cache{
+		items: make(map[string]CacheItem),
+	}
+}
+
+func NewCacheItem(body []byte, headers http.Header, status int) *CacheItem {
+	return &CacheItem{
+		Body:    body,
+		Headers: headers,
+		Status:  status,
+		created: time.Now(),
+	}
+}
+
+func (ci *CacheItem) Created() time.Time {
+	return ci.created
+}
+
+func (c *Cache) GetItem(key string) (CacheItem, bool) {
+	c.lock.RLock()
+	defer c.lock.RUnlock()
+	item, ok := c.items[key]
+	return item, ok
+}
+
+func (c *Cache) SetItem(key string, item CacheItem) {
+	c.lock.RLock()
+	defer c.lock.RUnlock()
+	c.items[key] = item
+}
+
+func (c *Cache) RemoveItem(key string) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	delete(c.items, key)
+}
+
+func (c *Cache) Items() map[string]CacheItem {
+	return c.items
+}
