2b8b7499ddda05912d76b3e8cac8052f4905d1c5
edaywalid <becharwalid9@gmail.com>
2024-09-25 14:34:33 +0100

    set up logging

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	log.Info().Msg("Hello, World!")
 }
